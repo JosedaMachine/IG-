f9e8614d87b3ee2013268106c3e30ae8bbc87238
Angelo06 <angelo06@ucm.es>
2021-04-30 00:20:07 +0200

    Tengo ganas de llorar cogido de las rodillas como si fuera Ovadillo después de volver de clase y darse cuenta de que no le gusta su trabajo ni su cuerpo ni sus gafas y que encima viste fatal

--- ProyectosIG_x64_VS2019_FG/IG1App/IndexMesh.cpp
+++ ProyectosIG_x64_VS2019_FG/IG1App/IndexMesh.cpp
@@ -4,47 +4,47 @@ void IndexMesh::render() const
 {
 	//Comandos OnpenGl para enviar datos de arrays a Gpu
 	//Nuevos comandos para la tabla de índices
-	
-	
+
+
    // transfer the coordinates of the vertices
-		glEnableClientState(GL_VERTEX_ARRAY);
-		glVertexPointer(3, GL_DOUBLE, 0, vVertices.data());  // number of coordinates per vertex, type of each coordinate, stride, pointer 
-		if (vColors.size() > 0) { // transfer colors
-			glEnableClientState(GL_COLOR_ARRAY);
-			glColorPointer(4, GL_DOUBLE, 0, vColors.data());  // components number (rgba=4), type of each component, stride, pointer  
-		}
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glVertexPointer(3, GL_DOUBLE, 0, vVertices.data());  // number of coordinates per vertex, type of each coordinate, stride, pointer 
+	if (vColors.size() > 0) { // transfer colors
+		glEnableClientState(GL_COLOR_ARRAY);
+		glColorPointer(4, GL_DOUBLE, 0, vColors.data());  // components number (rgba=4), type of each component, stride, pointer  
+	}
 
-		if (vTexCoords.size() > 0) { // transfer texture  data
-			glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-			glTexCoordPointer(2, GL_DOUBLE, 0, vTexCoords.data());  // components number (rgba=4), type of each component, stride, pointer  
-		}
-		// Comandos OpenGL para enviar datos de arrays a GPU
-		// Nuevos comandos para la tabla de índices
-		if (vIndices != nullptr) {
-			glEnableClientState(GL_INDEX_ARRAY);
-			glIndexPointer(GL_UNSIGNED_INT, 0, vIndices);
-		}
+	if (vTexCoords.size() > 0) { // transfer texture  data
+		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+		glTexCoordPointer(2, GL_DOUBLE, 0, vTexCoords.data());  // components number (rgba=4), type of each component, stride, pointer  
+	}
+	// Comandos OpenGL para enviar datos de arrays a GPU
+	// Nuevos comandos para la tabla de índices
+	if (vIndices != nullptr) {
+		glEnableClientState(GL_INDEX_ARRAY);
+		glIndexPointer(GL_UNSIGNED_INT, 0, vIndices);
+	}
 
-		if (vNormals.size() > 0) {
-			glEnableClientState(GL_NORMAL_ARRAY);
-			glNormalPointer(GL_DOUBLE, 0, vNormals.data());
-		}
+	if (vNormals.size() > 0) {
+		glEnableClientState(GL_NORMAL_ARRAY);
+		glNormalPointer(GL_DOUBLE, 0, vNormals.data());
+	}
 
-		draw();
+	draw();
 
-		glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-		glDisableClientState(GL_COLOR_ARRAY);
-		glDisableClientState(GL_VERTEX_ARRAY);
+	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+	glDisableClientState(GL_COLOR_ARRAY);
+	glDisableClientState(GL_VERTEX_ARRAY);
 
-		//Comandos OpenGL para deshabilitar datos enviados
-		//Nuevo comando para la tabla de índices
-		glDisableClientState(GL_INDEX_ARRAY);
-		glDisableClientState(GL_NORMAL_ARRAY);
+	//Comandos OpenGL para deshabilitar datos enviados
+	//Nuevo comando para la tabla de índices
+	glDisableClientState(GL_INDEX_ARRAY);
+	glDisableClientState(GL_NORMAL_ARRAY);
 }
 
-void IndexMesh::draw() const 
+void IndexMesh::draw() const
 {
-	
+
 	glDrawElements(mPrimitive, nNumIndices, GL_UNSIGNED_INT, vIndices);
 
 }
@@ -62,16 +62,16 @@ IndexMesh* IndexMesh::generaAnilloCuadradoIndexado(float outSide, float inSide)
 	m->vColors.reserve(m->mNumVertices);
 
 	//Colores								//Vértices								
-	m->vColors.emplace_back(0.0, 0.0, 0.0, 1.0); m->vVertices.emplace_back(30.0, 30.0, 0.0); 
-	m->vColors.emplace_back(1.0, 0.0, 0.0, 1.0); m->vVertices.emplace_back(10.0, 10.0, 0.0); 
-	m->vColors.emplace_back(0.0, 1.0, 0.0, 1.0); m->vVertices.emplace_back(70.0, 30.0, 0.0); 
-	m->vColors.emplace_back(0.0, 0.0, 1.0, 1.0); m->vVertices.emplace_back(90.0, 10.0, 0.0); 
-	m->vColors.emplace_back(1.0, 1.0, 0.0, 1.0); m->vVertices.emplace_back(70.0, 70.0, 0.0); 
-	m->vColors.emplace_back(1.0, 0.0, 1.0, 1.0); m->vVertices.emplace_back(90.0, 90.0, 0.0); 
-	m->vColors.emplace_back(0.0, 1.0, 1.0, 1.0); m->vVertices.emplace_back(30.0, 70.0, 0.0); 
-	m->vColors.emplace_back(1.0, 0.0, 0.0, 1.0); m->vVertices.emplace_back(10.0, 90.0, 0.0); 
-	m->vColors.emplace_back(0.0, 0.0, 0.0, 1.0); m->vVertices.emplace_back(30.0, 30.0, 0.0); 
-	m->vColors.emplace_back(1.0, 0.0, 0.0, 1.0); m->vVertices.emplace_back(10.0, 10.0, 0.0); 
+	m->vColors.emplace_back(0.0, 0.0, 0.0, 1.0); m->vVertices.emplace_back(30.0, 30.0, 0.0);
+	m->vColors.emplace_back(1.0, 0.0, 0.0, 1.0); m->vVertices.emplace_back(10.0, 10.0, 0.0);
+	m->vColors.emplace_back(0.0, 1.0, 0.0, 1.0); m->vVertices.emplace_back(70.0, 30.0, 0.0);
+	m->vColors.emplace_back(0.0, 0.0, 1.0, 1.0); m->vVertices.emplace_back(90.0, 10.0, 0.0);
+	m->vColors.emplace_back(1.0, 1.0, 0.0, 1.0); m->vVertices.emplace_back(70.0, 70.0, 0.0);
+	m->vColors.emplace_back(1.0, 0.0, 1.0, 1.0); m->vVertices.emplace_back(90.0, 90.0, 0.0);
+	m->vColors.emplace_back(0.0, 1.0, 1.0, 1.0); m->vVertices.emplace_back(30.0, 70.0, 0.0);
+	m->vColors.emplace_back(1.0, 0.0, 0.0, 1.0); m->vVertices.emplace_back(10.0, 90.0, 0.0);
+	m->vColors.emplace_back(0.0, 0.0, 0.0, 1.0); m->vVertices.emplace_back(30.0, 30.0, 0.0);
+	m->vColors.emplace_back(1.0, 0.0, 0.0, 1.0); m->vVertices.emplace_back(10.0, 10.0, 0.0);
 
 	//Indices
 	m->nNumIndices = 10;
@@ -120,42 +120,47 @@ IndexMesh* IndexMesh::generaCuboConTapadIndexado(GLdouble l)
 
 	m->vColors.reserve(m->mNumVertices);
 	//Vertices
-	m->vVertices.emplace_back(-l/2 , l / 2, l / 2);
-	m->vVertices.emplace_back(-l/2 , -l / 2, l / 2);
-	m->vVertices.emplace_back(l/2 , l / 2, l / 2);
+	m->vVertices.emplace_back(-l / 2, l / 2, l / 2);
+	m->vVertices.emplace_back(-l / 2, -l / 2, l / 2);
+	m->vVertices.emplace_back(l / 2, l / 2, l / 2);
 	m->vVertices.emplace_back(l / 2, -l / 2, l / 2);
 	m->vVertices.emplace_back(l / 2, l / 2, -l / 2);
 	m->vVertices.emplace_back(l / 2, -l / 2, -l / 2);
-	m->vVertices.emplace_back(-l / 2, l / 2,- l / 2);
+	m->vVertices.emplace_back(-l / 2, l / 2, -l / 2);
 	m->vVertices.emplace_back(-l / 2, -l / 2, -l / 2);
-	//Colores (Es verde)
-	m->vColors.emplace_back(0.0, 1.0, 0.0, 1.0);
-	m->vColors.emplace_back(0.0, 1.0, 0.0, 1.0);
-	m->vColors.emplace_back(0.0, 1.0, 0.0, 1.0);
-	m->vColors.emplace_back(0.0, 1.0, 0.0, 1.0);
-	m->vColors.emplace_back(0.0, 1.0, 0.0, 1.0);
-	m->vColors.emplace_back(0.0, 1.0, 0.0, 1.0);
-	m->vColors.emplace_back(0.0, 1.0, 0.0, 1.0);
-	m->vColors.emplace_back(0.0, 1.0, 0.0, 1.0);
-	m->vColors.emplace_back(0.0, 1.0, 0.0, 1.0);
 
 	//Indices
 	m->nNumIndices = 36;
 
-	m->vIndices = new GLuint[m->nNumIndices];
+	m->vIndices = new GLuint[m->nNumIndices]{ 
+		0,1,2,
+		2,1,3,
+		2,3,4,
+		4,3,5,
+		5,6,4,
+		5,7,6,
+		6,7,0,
+		0,7,1,
+		6,0,4,
+		4,0,2,
+		5,1,3,
+		7,1,5
+	};
 
-	m->vIndices[0] = 0;
-	m->vIndices[1] = 1;
-	m->vIndices[2] = 2;
-	m->vIndices[4] = 4;
-	m->vIndices[5] = 5;
-	m->vIndices[6] = 6;
-	m->vIndices[7] = 7;
+	m->vNormals.reserve(m->mNumVertices);
 
-
-	/*for (int i = 0; i < m->nNumIndices; i++) {
-		m->vIndices[i] = i % 8;
+	/*for (int i = 0; i < m->mNumVertices; i++) {
+		m->vNormals.push_back(glm::dvec3(0, 0, 0));
 	}*/
+	   
+	m->vNormals.emplace_back(glm::normalize(glm::dvec3(-1, 1, 1)));
+	m->vNormals.emplace_back(glm::normalize(glm::dvec3(-1, -1, 1)));
+	m->vNormals.emplace_back(glm::normalize(glm::dvec3(1, 1, 1)));
+	m->vNormals.emplace_back(glm::normalize(glm::dvec3(1, -1, 1)));
+	m->vNormals.emplace_back(glm::normalize(glm::dvec3(1, 1, -1)));
+	m->vNormals.emplace_back(glm::normalize(glm::dvec3(1, -1, -1)));
+	m->vNormals.emplace_back(glm::normalize(glm::dvec3(-1, 1, -1)));
+	m->vNormals.emplace_back(glm::normalize(glm::dvec3(-1, -1, -1)));
 
 	return m;
 }
